ba057255469fe099dda6f8885c347ad146b08996
Samuel Bastiat <samuel.bastiat@gmail.com>
2024-03-31 11:44:55 +0200

    Format black.

--- audit/audit.py
+++ audit/audit.py
@@ -10,38 +10,40 @@ app = Flask(__name__)
 
 def remplacer_caracteres(string: str) -> str:
     # Utiliser une expression régulière pour remplacer les caractères non autorisés par _
-    nouvelle_chaine = re.sub(r'[^a-zA-Z0-9_]', '_', string)
+    nouvelle_chaine = re.sub(r"[^a-zA-Z0-9_]", "_", string)
     return nouvelle_chaine
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('audit_fr.html')
+    return render_template("audit_fr.html")
 
 
-@app.route('/audit_fr.json')
+@app.route("/audit_fr.json")
 def get_audit_json():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'audit_fr.json')
+    return send_from_directory(os.path.join(app.root_path, "static"), "audit_fr.json")
 
 
-@app.route('/static/<path:filename>')
+@app.route("/static/<path:filename>")
 def static_file(filename):
-    return send_from_directory(os.path.join(app.root_path, 'static'), remplacer_caracteres(filename))
+    return send_from_directory(
+        os.path.join(app.root_path, "static"), remplacer_caracteres(filename)
+    )
 
 
-@app.route('/save_results', methods=['POST'])
+@app.route("/save_results", methods=["POST"])
 def save_results():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.json
         date_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-        filename = f'audit_{date_now}.json'
-        path = os.path.join(app.root_path, 'result', filename)
-        with open(path, 'w', encoding='utf-8') as file:
+        filename = f"audit_{date_now}.json"
+        path = os.path.join(app.root_path, "result", filename)
+        with open(path, "w", encoding="utf-8") as file:
             json.dump(data, file, indent=2)
 
         return jsonify({"status": "success", "filename": filename})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
